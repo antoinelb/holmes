8b59a1077515c1bcb083f658bfbe5cc32040add6
Antoine Lefebvre-Brossard <antoinelb@proton.me>
2026-03-24 10:04:03 -0400

    wip

---
+++ scripts/run_all_experiments.py
@@ -0,0 +1,12 @@
+"""
+Runs calibration on all catchments and models (with cemaneige) to verify their
+correctness.
+"""
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
